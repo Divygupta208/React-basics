58bdcdef0c724e469725178c1333bd7abfd01359
Divy Gupta <divygupta208@gmail.com>
2023-12-17 13:19:44 +0530

    first

---
+++ expense-app/src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItems from "./components/ExpenseItems";
+
+
+function App() {
+  return (
+   <>
+    <ExpenseItems></ExpenseItems>
+   </>
+  );
+}
+
+export default App;
---
+++ expense-app/src/components/ExpenseItems.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function ExpenseItems() {
+  return (
+    
+    <div>
+        <h1>Expense Items</h1>
+        <h3>Food Rs-10</h3>
+        <h3>Petrol Rs-100</h3>
+        <h3>Movie Rs-200</h3>
+    </div>
+
+  )
+}
+
+export default ExpenseItems
