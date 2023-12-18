26e0ee0a7e65420f9df636f847d4894a7c41511b
Divy Gupta <divygupta208@gmail.com>
2023-12-18 19:06:40 +0530

    added css and uses js

--- expense-app/src/components/ExpenseItems.js
+++ expense-app/src/components/ExpenseItems.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React from "react";
+import "./ExpenseItems.css";
 
 function ExpenseItems() {
+  const expenseDate = new Date(2023, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 297.67;
+  const expenseLocation = "N-21 , S-41 , New York";
   return (
-    
-    <div>
-        <h1>Expense Items</h1>
-        <h3>Food Rs-10</h3>
-        <h3>Petrol Rs-100</h3>
-        <h3>Movie Rs-200</h3>
+    <div className="expense-item">
+      <h1>
+        {`${expenseDate.getDate()}-
+          ${expenseDate.getDay()}-
+          ${expenseDate.getFullYear()}`}
+      </h1>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-location">{expenseLocation}</div>
+        <div className="expense-item__price">{expenseAmount}</div>
+      </div>
     </div>
-
-  )
+  );
 }
 
-export default ExpenseItems
+export default ExpenseItems;
