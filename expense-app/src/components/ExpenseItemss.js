fed4851aed275a0b578add58b78a770c8ecdd6c9
Divy Gupta <divygupta208@gmail.com>
2024-01-06 12:22:01 +0530

    filter

--- expense-app/src/components/ExpenseItemss.js
+++ expense-app/src/components/ExpenseItemss.js
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItems from "./ExpenseItems";
 import "./Expenses.css";
 import Card from "./UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 const ExpenseItemss = ({ expenses }) => {
+  const [filteredYear, setFilteredYear] = useState("2022");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
     <Card className="expenses">
-      {expenses.map((item) => (
-        <ExpenseItems
-          key={item.title}
-          expenseDate={item.date}
-          expenseTitle={item.title}
-          expenseAmount={item.amount}
-          expenseLoc={item.location}
-        ></ExpenseItems>
-      ))}
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      >
+        {expenses.map((item) => (
+          <ExpenseItems
+            key={item.title}
+            expenseDate={item.date}
+            expenseTitle={item.title}
+            expenseAmount={item.amount}
+            expenseLoc={item.location}
+          ></ExpenseItems>
+        ))}
+      </ExpensesFilter>
     </Card>
   );
 };
