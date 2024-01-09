bffefd530b60d77b38cbb81f6c65da1e40f532eb
Divy Gupta <divygupta208@gmail.com>
2024-01-10 02:04:16 +0530

    expense tracker done

--- expense-app/src/components/ExpenseItemss.js
+++ expense-app/src/components/ExpenseItemss.js
@@ -4,6 +4,7 @@ import "./Expenses.css";
 import Card from "./UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const ExpenseItemss = ({ expenses }) => {
   const [filteredYear, setFilteredYear] = useState("2022");
@@ -22,6 +23,7 @@ const ExpenseItemss = ({ expenses }) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList filteredExpenses={filteredExpenses} />
     </Card>
   );
